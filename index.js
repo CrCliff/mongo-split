e8f16ce39fda98fb37c249e503e1f29a9fd9bd91
CrCliff <crcliff@comcast.net>
2018-12-28 16:57:27 -0500

    Version 1.1.0:
    Now usable to split collection by key. Does not consider overlap. If a
    document already exists, it simply skips that document.

--- index.js
+++ index.js
@@ -2,8 +2,37 @@
 // 12/2018
 //
 // Entry for mongo_split module
-//
+// Pass the module a collection and a key, the module creates a bunch of seperate collections.
 
-exports.printMsg = function() {
-	console.log('This is mongo_split.');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+exports.splitCollection = function(url, dbName, collectionName, key) { 
+
+	MongoClient.connect(url, (connectErr, client) => {
+		assert.equal(connectErr, null);
+
+		var db = client.db(dbName);
+		var collection = db.collection(collectionName);
+
+		collection.countDocuments( (countErr, count) => {
+			assert.equal(countErr, null);
+			assert.notEqual(count, 0, 'Collection is empty.');
+		});
+
+		var cursor = collection.find();
+		cursor.forEach( (doc) => {
+			assert.notEqual(doc, null);
+
+			var subCollection = db.collection(String(doc[key]));
+
+			subCollection.insertOne(doc, (insertErr, res) => {
+				if (insertErr) {
+					console.log("Inserting duplicate order attempted...");
+				}
+			})
+
+		});
+
+	});
 }
